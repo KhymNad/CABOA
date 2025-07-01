2c0e3832d6b79fe6d8e5e579f39fd80fe2fcba2a
KhymNad <87257632+KhymNad@users.noreply.github.com>
2025-06-30 20:04:11 -0600

    updated navbar functionality

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './NavBar.module.css'; // Ensure you have a CSS file for styling
+import { NavLink, Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
     <nav className={styles.navbar}>
-        <div className={styles.logoContainer}>
+      <div className={styles.logoContainer}>
+        <Link to="/">
           <img src="/img/caboa_logo.png" alt="CABOA Logo" className={styles.logo} />
-        </div>
-        <ul className={styles.navLinks}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/resources">Resources</Link></li>
-            <li><Link to="/events">Events/News</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
+        </Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <NavLink to="/" className={({ isActive }) => isActive ? styles.activeLink : ''}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/resources" className={({ isActive }) => isActive ? styles.activeLink : ''}>
+            Resources
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/events" className={({ isActive }) => isActive ? styles.activeLink : ''}>
+            Events/News
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={({ isActive }) => isActive ? styles.activeLink : ''}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
